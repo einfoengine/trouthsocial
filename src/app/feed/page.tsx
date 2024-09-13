5a21cbc89d29f013dab8e910541f4c86ab1c4f3e
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2024-09-13 03:24:20 +0300

    Styling feeds: Creat feed added, space adjusted. Feed impressions and actions to be styled

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -37,6 +37,13 @@ const Feed = () => {
 
     return (
         <div className='ntcom nt-feeds'>
+            <div className="ntcf">
+                <Avtr src="https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                <span className="ntcfi">Whats in your mind? Let the people know...</span>
+                {/* <div className="ntfa">
+                    
+                </div> */}
+            </div>
             {feeds.map((feed, index) => (
                 <div key={"ntfd"+index} className="ntfd">
                     <div className="ntpi">
@@ -52,15 +59,16 @@ const Feed = () => {
                     {/* nodes theme post information */}
                     <div className="ntft">
                         <p>{feed.text}</p>
+                        <div className="ntac">
+                            <span>View {feed.reactions[0]}</span>
+                            <span>Impression {feed.reactions[1]}</span>
+                            <span>Shared {feed.reactions[2]}</span>
+                            <span>Comment {feed.reactions[3]}</span>
+                        </div>
                     </div>
                 </div>
             ))}
-            <div className="ntcf">
-                <input className="ntcfi" type="text"/>
-                <div className="ntfa">
-                    
-                </div>
-            </div>
+            
         </div>
     )
 }
